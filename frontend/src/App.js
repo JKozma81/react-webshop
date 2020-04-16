cee39518e5e55161ebe45589064e690cd36f9fe2
JKozma81 <janos.kozma81@gmail.com>
2020-04-16 17:50:37 +0200

    sceleton frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Header from './components/Header';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <div className="site-content">
+          <Switch>
+            <Route exact path="/">
+              <h1>Home</h1>
+            </Route>
+            {/* <Route
+              path="/blog/:id"
+              component={(routProps) => (
+                <PostDetail postId={routProps.match.params.id} />
+              )}
+            ></Route> */}
+            <Route path="/products">Products</Route>
+            <Route path="/orders">Orders</Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="d-flex flex-row align-center justify-center">
+        <div className="d-flex flex-column justify-center align-center">
+          <h1> Our React Webshop</h1>
+        </div>
+        <nav className="d-flex">
+          <ul className="navlinks d-flex flex-row justify-evenly align-center">
+            <li>
+              <NavLink
+                className="navlink d-flex justify-center align-center"
+                exact
+                activeClassName="selected"
+                to="/"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="navlink d-flex justify-center align-center"
+                activeClassName="selected"
+                to="/products"
+              >
+                Products
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="navlink d-flex justify-center align-center"
+                activeClassName="selected"
+                to="/orders"
+              >
+                Orders
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
