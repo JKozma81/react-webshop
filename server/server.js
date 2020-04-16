31148fb3783aaffd6dbebb43e9c63013534f5f78
KriszNyikos <krisz.nyikos@gmail.com>
2020-04-16 19:32:48 +0200

    database and path fixed

--- server/server.js
+++ server/server.js
@@ -5,46 +5,46 @@ const port = 5000
 let cors = require('../frontend/node_modules/cors')
 
 const posts = [
-    {
+    {    'id':1,
         "name": "Intel i5 3600U",
         "shortSpecs": "i5 3Ghz",
-        "image": "https://p1.akcdn.net/full/566327412.intel-core-i5-9600kf-hexa-core-3-70ghz-lga1151.jpg",
+        "image": "/images/product-01/i5.jpg",
         "qty": 4,
         "url": "https://ark.intel.com/content/www/us/en/ark/products/88190/intel-core-i5-6300u-processor-3m-cache-up-to-3-00-ghz.html",
         "price": 74
       
       },
-      {
+      { 'id':2,
         "name": "Radeon Ryzen 5600",
         "shortSpecs": "Ryzen 5600",
-        "image": "https://s12emagst.akamaized.net/products/5414/5413419/images/res_e058e84cc16e0c0bc182d264a7b475f3_450x450_bdvg.jpg",
+        "image": "/images/product-02/ryzen.jpg",
         "qty": 7,
         "url": "https://www.amd.com/en/ryzen-5",
         "price": 34
       
       },
-      {
+      { 'id':3,
         "name": "Asus Vivobook x6524",
         "shortSpecs": "Vivobook 12 GB",
-        "image": "https://s12emagst.akamaized.net/products/21645/21644225/images/res_ae52dabb75678ef268faf3aa7ea4ce10_450x450_ogg8.jpg",
+        "image": "/images/product-03/asus.jpg",
         "qty": 4,
         "url": "https://www.asus.com/hu/Laptops/VivoBook-Series-Products/",
         "price": 14
       
       },
-      {
+      { 'id':4,
         "name": "Laptop hűtő",
         "shortSpecs": "Olcsó laptop hűtő",
-        "image": "https://www.bestmarkt.hu/img/o/411392-2bk9b.jpg",
+        "image": "/images/product-04/huto.jpg",
         "qty": 12,
         "url": "https://www.bestmarkt.hu/cooler-master-notepal-l2-17-laptop-hutopad-fekete-p411392",
         "price": 2
       
       },
-      {
+      { 'id':5,
         "name": "Creative Hangszóró",
         "shortSpecs": "Creative 6534",
-        "image": "https://p1.akcdn.net/full/329993267.creative-e-mu-xm7-2-0.jpg",
+        "image": "/images/product-05/creative.jpg",
         "qty": 20,
         "url": "https://us.creative.com/p/speakers/e-mu-xm7-bookshelf-speakers",
         "price": 24
@@ -61,14 +61,14 @@ var db = new sqlite3.Database(':memory:');
 app.use(cors())
 
 db.serialize(function () {
-    db.run("CREATE TABLE products('name' TEXT, 'shortSpec' TEXT, 'image' TEXT, 'qty' INT, 'url' TEXT, 'price' INT);");
+    db.run("CREATE TABLE products('id' TEXT, 'name' TEXT, 'shortSpec' TEXT, 'image' TEXT, 'qty' INT, 'url' TEXT, 'price' INT);");
 })
 
 db.serialize(function () {
     posts.forEach(post => {
-       let  {name, shortSpecs, image, qty, url, price} = post
-        db.prepare(`INSERT INTO products VALUES (?,?,?,?,?,?)`)
-            .run(name, shortSpecs, image, qty, url, price)
+       let  {id, name, shortSpecs, image, qty, url, price} = post
+        db.prepare(`INSERT INTO products VALUES (?,?,?,?,?,?,?)`)
+            .run(id,name, shortSpecs, image, qty, url, price)
     })
 })
 
@@ -77,7 +77,7 @@ app.get('/products', (req, res) => {
 
  
         db.serialize(function () {
-            db.all('SELECT rowid as id, name, shortSpec, image, qty, url FROM products', function (err, result) {
+            db.all('SELECT * FROM products', function (err, result) {
                 if (err != null) {
                     // hibakezelés
                 }
