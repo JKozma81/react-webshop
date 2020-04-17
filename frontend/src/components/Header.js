c4f1bfe63a0de40f424fc2527811745eef1dee6a
JKozma81 <janos.kozma81@gmail.com>
2020-04-17 02:22:44 +0200

    surprise

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,46 +1,21 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 export default class Header extends Component {
   render() {
     return (
-      <div className="d-flex flex-row align-center justify-between padding-15">
-        <div className="d-flex flex-column justify-center align-center">
-          <h1> Our React Webshop</h1>
-        </div>
-        <nav className="">
-          <ul className="navlinks d-flex flex-row justify-end align-center">
-            <li>
-              <NavLink
-                className="navlink d-flex justify-center align-center"
-                exact
-                activeClassName="selected"
-                to="/"
-              >
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                className="navlink d-flex justify-center align-center"
-                activeClassName="selected"
-                to="/products"
-              >
-                Products
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                className="navlink d-flex justify-center align-center"
-                activeClassName="selected"
-                to="/orders"
-              >
-                Orders
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">Our React Webshop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/products">Products</Nav.Link>
+            <Nav.Link href="/orders">Orders</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
